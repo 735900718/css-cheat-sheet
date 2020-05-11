33ea9f5bdeca0a9029ecdfb1fa6d000622cd0da5
梁玉珂 <liangyk@autoai.com>
2020-05-11 15:21:40 +0800

    修改链接地址

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,9 @@ module.exports = {
   title: 'CSS 样式小抄',
   description: '一些CSS样式积累备用',
   themeConfig: {
+    nav: [
+      { text: 'github', link: 'https://github.com/735900718/css-cheat-sheet' },
+    ],
     sidebar: {
       '/': [
         {
